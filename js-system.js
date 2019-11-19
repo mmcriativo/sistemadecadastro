9288c3ccd1c67d05348695e74ffe9f131ef79ac8
Mateus Monteiro <mateus@mmcriativo.com.br>
2019-11-19 15:17:32 -0300

    Primeiro arquivos

---
+++ js-system.js
@@ -0,0 +1,139 @@
+        function esconderAsideLeft(){
+                var aside = document.getElementById("aside-left");
+                
+                if (aside.style.display === "none"){
+                    aside.style.display = "block";
+                } else {
+                    aside.style.display = "none";
+                }
+            }
+            
+            function dropCadastro(){
+                var cadastro = document.getElementById("cadastro");
+                
+                if (cadastro.style.display === "none"){
+                    cadastro.style.display = "block";
+                } else {
+                    cadastro.style.display = "none";
+                }
+            }
+            
+            function dropProdutos(){
+                var produtos = document.getElementById("produtos");
+                
+                if (produtos.style.display === "none"){
+                    produtos.style.display = "block";
+                } else {
+                    produtos.style.display = "none";
+                }
+            }
+            
+            function dropServicos(){
+                var servicos = document.getElementById("servico");
+                
+                if (servicos.style.display === "none"){
+                    servicos.style.display = "block";
+                } else {
+                    servicos.style.display = "none";
+                }
+            }
+            
+            function dropOrcamento(){
+                var orcamento = document.getElementById("orcamento");
+                
+                if (orcamento.style.display === "none"){
+                    orcamento.style.display = "block";
+                } else {
+                    orcamento.style.display = "none";
+                }
+            }
+            
+            function dropOrdservico(){
+                var ordServico = document.getElementById("ord-servico");
+                
+                if (ordServico.style.display === "none"){
+                    ordServico.style.display = "block";
+                } else {
+                    ordServico.style.display = "none";
+                }
+            }
+            
+            function dropVendas(){
+                var vendas = document.getElementById("vendas");
+                
+                if (vendas.style.display === "none"){
+                    vendas.style.display = "block";
+                } else {
+                    vendas.style.display = "none";
+                }
+            }
+            
+            function dropEstoque(){
+                var estoque = document.getElementById("estoque");
+                
+                if (estoque.style.display === "none"){
+                    estoque.style.display = "block";
+                } else {
+                    estoque.style.display = "none";
+                }
+            }
+            
+             function dropFinanceiro(){
+                var financeiro = document.getElementById("financeiro");
+                
+                if (financeiro.style.display === "none"){
+                    financeiro.style.display = "block";
+                } else {
+                   financeiro.style.display = "none";
+                }
+            }
+            
+            function dropNfs(){
+                var nfs = document.getElementById("nfs");
+                
+                if (nfs.style.display === "none"){
+                    nfs.style.display = "block";
+                } else {
+                   nfs.style.display = "none";
+                }
+            }
+            
+            function dropContratos(){
+                var contratos = document.getElementById("contratos");
+                
+                if (contratos.style.display === "none"){
+                    contratos.style.display = "block";
+                } else {
+                   contratos.style.display = "none";
+                }
+            }
+            
+            function dropAtendimento(){
+                var atendimento = document.getElementById("atendimento");
+                
+                if (atendimento.style.display === "none"){
+                    atendimento.style.display = "block";
+                } else {
+                   atendimento.style.display = "none";
+                }
+            }
+            
+            function dropRelatorio(){
+                var relatorio = document.getElementById("relatorios");
+                
+                if (relatorio.style.display === "none"){
+                    relatorio.style.display = "block";
+                } else {
+                   relatorio.style.display = "none";
+                }
+            }
+            
+            function dropConfig(){
+                var config = document.getElementById("config");
+                
+                if (config.style.display === "none"){
+                    config.style.display = "block";
+                } else {
+                   config.style.display = "none";
+                }
+            }
